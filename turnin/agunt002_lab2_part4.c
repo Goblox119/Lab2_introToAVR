0ba29411929c2b6f12e36f97cae756a693848743
Goblox119 <gunter.am@gmail.com>
2021-01-20 04:29:42 -0800

    Completed part 4

---
+++ turnin/agunt002_lab2_part4.c
@@ -0,0 +1,41 @@
+/*	Author: Adam Gunter
+ *  Partner(s) Name: Jo Smith
+ *	Lab Section: 21
+ *	Assignment: Lab #2  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
+    DDRC = 0x00; PORTC = 0xFF; // Configure port C's 8 pins as inputs
+    DDRD = 0xFF; PORTD = 0x00; // Configure port D's 8 pins as outputs, initialize to 0's
+    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+    unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+    unsigned char tmpC = 0x00; // Temporary variable to hold the value of C
+    unsigned char sum; // Temporary variable to hold the value of D
+    /* Insert your solution below */
+    while (1) {
+	// 1) Read input
+	tmpA = PINA;
+	tmpB = PINB;
+	tmpC = PINC;
+	// 2) Perform computation
+	sum = tmpA + tmpB + tmpC;
+	if (sum > 511) { sum = sum >> 2; }
+	if (sum > 255) { sum = sum >> 1; }
+	if (sum > 140) { sum = sum | 0x01; }
+	if ((tmpA - tmpC) > 80 || ((tmpC - tmpA) > 80)) { sum = sum | 0x02; }
+	// 3) Write output
+	PORTD = sum;
+    }
+    return 0;
+}
